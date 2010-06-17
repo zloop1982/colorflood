25199350bec8f14fc6cacbaafa9d07f51621d2f5
Serge Ziryukin <ftrvxmtrx@gmail.com>
2010-06-17 20:06:02 +0300

    German translation by Falko Thomale

---
+++ src/resources/tr/de_DE.ts
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>Board</name>
+    <message>
+        <location filename="../../board.cpp" line="319"/>
+        <source>You win!
+You have used %1 turns out of %2.</source>
+        <extracomment>win message</extracomment>
+        <translation type="unfinished">Sie haben Gewonnen!
+Sie haben %1 von %2 Zügen benötigt.</translation>
+    </message>
+    <message>
+        <location filename="../../board.cpp" line="327"/>
+        <source>You lose!</source>
+        <extracomment>fail message</extracomment>
+        <translation type="unfinished">Sie haben verloren!</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../scheme.cpp" line="37"/>
+        <source>Default scheme</source>
+        <oldsource>Default</oldsource>
+        <extracomment>default scheme name</extracomment>
+        <translation type="unfinished">Standard Schema</translation>
+    </message>
+    <message>
+        <location filename="../../scheme.cpp" line="47"/>
+        <source>Black-and-white scheme</source>
+        <oldsource>Black-and-white</oldsource>
+        <extracomment>black-and-white scheme name</extracomment>
+        <translation type="unfinished">Schwarz-Weiß Schema</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../../window.cpp" line="68"/>
+        <source>New game</source>
+        <translation type="unfinished">Neues Spiel</translation>
+    </message>
+    <message>
+        <location filename="../../window.cpp" line="103"/>
+        <source>Help</source>
+        <translation type="unfinished">Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../../window.cpp" line="121"/>
+        <source>Less cells</source>
+        <translation type="unfinished">Weniger Zellen</translation>
+    </message>
+    <message>
+        <location filename="../../window.cpp" line="128"/>
+        <source>More cells</source>
+        <translation type="unfinished">Mehr Zellen</translation>
+    </message>
+    <message>
+        <location filename="../../window.cpp" line="147"/>
+        <source>Turns: %1/%2</source>
+        <extracomment>number of turns</extracomment>
+        <translation type="unfinished">Züge: %1/%2</translation>
+    </message>
+    <message>
+        <location filename="../../window.cpp" line="198"/>
+        <source>The object of the game is to turn a board into one single color. Number of moves is limited. You start from top-left corner with one cell already flooded.
+Good luck!</source>
+        <translation type="unfinished">Ziel des Spiel ist es, das ganze Feld in eine Farbe zu wandeln. Dabei sind die Züge begrenzt. Sie beginnen in der linken oberen Ecke mit einer bereits gefüllten Zelle.
+Viel Glück!</translation>
+    </message>
+    <message>
+        <location filename="../../window.cpp" line="264"/>
+        <source>Best record: %1/%2</source>
+        <extracomment>best number of turns</extracomment>
+        <translation type="unfinished">Rekord: %1/%2</translation>
+    </message>
+    <message>
+        <location filename="../../window.cpp" line="293"/>
+        <source>Games won: %1/%2</source>
+        <extracomment>games won/played</extracomment>
+        <translation type="unfinished">Spiele gewonnen: %1/%2</translation>
+    </message>
+    <message>
+        <location filename="../../window.hpp" line="62"/>
+        <source>Left-handed mode</source>
+        <translation type="unfinished">Linkshänder Modus</translation>
+    </message>
+    <message>
+        <location filename="../../window.hpp" line="63"/>
+        <source>Right-handed mode</source>
+        <translation type="unfinished">Rechtshänder Modus</translation>
+    </message>
+</context>
+</TS>
